ffb1fc6936bb7420b87c34389d9c03b4b785680f
26tanisha <tanishakeskar@gmail.com>
2026-01-16 18:43:39 +0530

    Add movie recommendation system with Streamlit UI
    Implemented a movie recommendation system using Streamlit.

---
+++ movie-recommendation-system.py
@@ -0,0 +1,35 @@
+import streamlit as st
+import pandas as pd
+import pickle
+
+# Load data
+movies_dict = pickle.load(open('movie_dict.pkl', 'rb'))
+movies = pd.DataFrame(movies_dict)
+similarity = pickle.load(open('similarity.pkl', 'rb'))
+
+def recommend(movie):
+    movie_index = movies[movies['title'] == movie].index[0]
+    distances = similarity[movie_index]
+
+    movie_list = sorted(
+        list(enumerate(distances)),
+        reverse=True,
+        key=lambda x: x[1]
+    )[1:6]
+
+    return [movies.iloc[i[0]].title for i in movie_list]
+
+# Streamlit UI
+st.title("🎬 Movie Recommendation System")
+
+selected_movie = st.selectbox(
+    "Select a movie:",
+    movies["title"].values
+)
+
+if st.button("Recommend"):
+    recommendations = recommend(selected_movie)
+
+    st.subheader("Recommended Movies:")
+    for movie in recommendations:
+        st.write("•", movie)
